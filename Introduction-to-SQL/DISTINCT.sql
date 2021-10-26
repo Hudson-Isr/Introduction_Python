262095e0d8f9955357686ef70124479852be1ef4
Hudson Israel <92607068+Hudson-Isr@users.noreply.github.com>
2021-10-26 10:16:41 -0300

    Introduction-to-SQL

--- Introduction-to-SQL/DISTINCT.sql
+++
@@ -1,5 +0,0 @@
--- Quantos nomes e sobrenomes exclusivos temos em nossa tabela
--- How many unique first and last names do we have in our table person.person
-
-SELECT DISTINCT FirstName, LastName
-FROM Person.Person
